1d4380e85735a27da5c1478f554344604b95ec5f
Nebraskinator <ruhe@ucsb.edu>
2023-12-17 14:18:54 -0800

    updated test

--- test.py
+++ test.py
@@ -2,12 +2,18 @@
 from simpletft.tft import SimpleTFT
 import numpy as np
 
-env = SimpleTFT('./log.txt')
 
-for i in range(10000):
-    obs, taking_actions, action_masks = env.reset()
-    dones = {p: False for p in env.live_agents}
-    while not all(dones.values()):
-        action = {p: np.random.choice(np.flatnonzero(mask)) for p, mask in action_masks.items()}
-        obs, rewards, taking_actions, dones, action_masks = env.step(action)
+if __name__ == "__main__":
+
+
+    env = SimpleTFT()
+    
+    for i in range(1000):
+        obs, taking_actions, action_masks = env.reset()
+        dones = {p: False for p in env.live_agents}
+        while not all(dones.values()):
+            action = {p: np.random.choice(np.flatnonzero(mask)) for p, mask in action_masks.items()}
+            obs, rewards, taking_actions, dones, action_masks = env.step(action)
+
+    print("successfully completed 1000 games with random actions")
         
